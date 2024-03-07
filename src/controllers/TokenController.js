975b85857ad181b7f972c99b7b5bfb7a9fc1f150
Matheus Levi <matheuslevit@gmail.com>
2024-03-07 19:01:22 -0300

    translate token controller

--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -3,35 +3,35 @@ import jwt from 'jsonwebtoken';
 
 class TokenController {
   async store(req, res) {
-    const {email = '', password = ''}  = req.body;
+    const { email = '', password = '' } = req.body;
 
-    if(!email || !password) {
+    if (!email || !password) {
       return res.status(401).json({
-        errors: ['Credenciais inválidas'],
-      })
+        errors: ['Invalid credentials'],
+      });
     }
 
-    const user = await User.findOne({where: { email }});
+    const user = await User.findOne({ where: { email } });
 
-    if(!user) {
+    if (!user) {
       return res.status(401).json({
-        errors: ['Usuário não existe'],
-      })
-    };
+        errors: ['User does not exist'],
+      });
+    }
 
-    if(!(await user.passwordIsValid(password))) {
+    if (!(await user.passwordIsValid(password))) {
       return res.status(401).json({
-        errors: ['Senha incorreta'],
-      })
-    };
+        errors: ['Incorrect password'],
+      });
+    }
 
     const { id } = user;
     const token = jwt.sign({ id, email }, process.env.TOKEN_SECRET, {
-      expiresIn: process.env.TOKEN_EXPIRATION
+      expiresIn: process.env.TOKEN_EXPIRATION,
     });
 
-    return res.json({ token, user: { nome: user.nome, id, email} });
-  };
-};
+    return res.json({ token, user: { name: user.name, id, email } });
+  }
+}
 
 export default new TokenController();
